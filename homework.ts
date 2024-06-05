3924f38d49d9aa921821854742195054c34f1b82
Ricardokim94 <cmrikim@naver.com>
2024-06-05 20:24:08 +0900

    typescript 과제 두문제 해보기

---
+++ homework.ts
@@ -0,0 +1,30 @@
+// (숙제1) 이름을 파라미터로 입력하면 콘솔창에 "안녕하세요 홍길동"을 출력해주고
+// 아무것도 파라미터로 입력하지 않고 함수를 사용하면 "이름이 없습니다" 를 출력하는 함수를 만들어봅시다.
+// 파라미터와 return 타입지정도 잘 해봅시다. 
+
+function greet(x?: string) : void{
+    if(x){ 
+        console.log(`안녕하세요. ${x} 입니다.`);
+    }else {
+        console.log('이름이 없습니다')
+    }
+}
+
+greet('김창목');
+
+// (숙제2) 함수에 숫자 또는 문자를 집어넣으면 자릿수를 세어 출력해주는 함수를 만들어보십시오.
+// 예를 들어 '245' 이런 문자를 입력하면 3이 return 되어야합니다.
+// 숫자도 마찬가지로 9567 이런 숫자를 입력하면 4가 return 되어야합니다.
+// 숫자 또는 문자 이외의 자료가 들어오면 안됩니다. 
+
+function count(x: number | string): number {
+    if (typeof x === 'string' || typeof x === 'number') {
+        return x.toString().length;
+    } else {
+        throw new Error('숫자 또는 문자 이외의 자료가 들어오면 안됩니다.');
+    }
+}
+
+console.log(count('245')); // 출력: 3
+console.log(count(9567));  // 출력: 4
+console.log(count('창목'));  // 출력: 2
--- index.js
+++ index.js
@@ -8,3 +8,13 @@ rename = 123;
 rename = 'changmok';
 var renmae2; //any 랑 같은 의미 /unknown이 조금더 안정성이 있다.
 //renmae2 -1    unknown type은 뺄셈을 못한다. 미연에 방지.
+// //function 에다가 type 주는 법
+// function test2(x: number|string) : void{ //return type지정은 함수() : number 여기다가 한다.
+//     if(x === ){
+//         console.log(x + 3);
+//     }
+// } 
+// test2(30);
+// (숙제1) 이름을 파라미터로 입력하면 콘솔창에 "안녕하세요 홍길동"을 출력해주고
+// 아무것도 파라미터로 입력하지 않고 함수를 사용하면 "이름이 없습니다" 를 출력하는 함수를 만들어봅시다.
+// 파라미터와 return 타입지정도 잘 해봅시다. 
