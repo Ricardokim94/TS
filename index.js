8d161659ed83d26bb6c25ce3c9f0d91f17c51c33
Ricardokim94 <cmrikim@naver.com>
2024-06-05 21:24:34 +0900

    Narrowing & Assertion 및 콘솔에 출력하는 방법

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ rename = 123;
 rename = 'changmok';
 var renmae2; //any 랑 같은 의미 /unknown이 조금더 안정성이 있다.
 //renmae2 -1    unknown type은 뺄셈을 못한다. 미연에 방지.
-// //function 에다가 type 주는 법
-// function test2(x: number|string) : void{ //return type지정은 함수() : number 여기다가 한다.
-//     if(x === ){
-//         console.log(x + 3);
-//     }
-// } 
-// test2(30);
-// (숙제1) 이름을 파라미터로 입력하면 콘솔창에 "안녕하세요 홍길동"을 출력해주고
-// 아무것도 파라미터로 입력하지 않고 함수를 사용하면 "이름이 없습니다" 를 출력하는 함수를 만들어봅시다.
-// 파라미터와 return 타입지정도 잘 해봅시다. 
+//function 에다가 type 주는 법
+function test2(x) {
+    ////Narrowing     
+    // if(typeof x === 'string'){  //if 를 줘서 typeof 만들어 사용
+    //     return x+1
+    // }else{
+    // }
+    ////Assertion (type 덮어쓰기)
+    var array = [];
+    array[0] = x; //as를 사용해서 number로 덮어씀
+}
+console.log(test2(30));
+//assertion 용도
+// 1. Narrowing 할때 사용 (변경할 때 사용하는 문법이 아님.)
+// 2. 어떤 타입이 들어올지 100% 알때 사용하는 것임.
