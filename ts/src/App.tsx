71c0b6fa2e733005bae5850f0de43e430a7dbde4
Ricardokim94 <cmrikim@naver.com>
2024-06-12 12:55:15 +0900

    react typescript - component 와 usestate에서 사용해보기

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type User={
+  name : string;
+  age : string;
+}
+
 function App() {
+
+  let [user, setUser] = useState('kim')
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>안녕하십니까</h4>
+      <Porfile name='철수' age='20'></Porfile>
     </div>
   );
 }
+
+
+function Porfile(props: User): JSX.Element { //html 로 return 한다 : JSX.Element로 하면 된다.
+  return (
+      <div>{props.name} 입니다. 나이는 {props.age} 입니다.</div>
+  )
+}
+
 
 export default App;
