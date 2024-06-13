a2bedbd534abe4b08f78336faa18c7e1b70bdc7a
Ricardokim94 <cmrikim@naver.com>
2024-06-13 17:10:59 +0900

    index signature

--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
-let 이름 : string = 'kim';
-let 나이 : Age;
+//index signature 이란 모든타입이 string 일때 하나하나 다 string 하기 귀찮으니까 한번에 type 주는 것 
+interface StringOnly {
+    [key :string] : string|number //이렇게 사용함.
+   
+}
+let User = {
+    name : 'kim',
+    age : 20,
+    location : 'seoul'
+}
+////////////////////////////////////////////////////////////////////////////////
+//이게 조금 더 정확한 방법
+interface StringOnly2 {
+    [key :number] : string //이렇게 사용함.
+}
+let User2 = {
+    0 : 'kim',
+    1 : '20',
+    2 : 'seoul'
+}
